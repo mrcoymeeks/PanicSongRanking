3022a08acff0fb5eae84c98ee28df66cae0191b4
Coy Meeks <cmeeks@AWP-CLT-0577W.awarepoint.com>
2018-07-26 20:35:44 -0400

    Add project files.

---
+++ PanicSongRanking/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using PanicSongRanking.Models;
+
+namespace PanicSongRanking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Song> panicsongList = new List<Song>();
+            panicsongList = BuildPanicSongList();
+            ViewBag.PanicSongList = panicsongList;
+
+            List<Song> phishSongList = new List<Song>();
+            phishSongList = BuildPhishSongList();
+            ViewBag.PhishSongList = phishSongList;
+
+            return View();
+        }
+
+        private List<Song> BuildPanicSongList()
+        {
+            var masterList = System.IO.File.ReadAllLines(HostingEnvironment.MapPath(@"~/App_Data/SongList.txt"));
+            List<Song> songs = new List<Song>();
+            int id = 1;
+
+            foreach (var song in masterList)
+            {
+                songs.Add(new Song(id, song));
+                id++;
+            }
+            return songs;
+        }
+
+        private List<Song> BuildPhishSongList()
+        {
+            var masterList = System.IO.File.ReadAllLines(HostingEnvironment.MapPath(@"~/App_Data/PhishSongList.txt"));
+            List<Song> songs = new List<Song>();
+            int id = 1;
+
+            foreach (var song in masterList)
+            {
+                songs.Add(new Song(id, song));
+                id++;
+            }
+            return songs;
+        }
+    }
+}
---
+++ PanicSongRanking/Models/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PanicSongRanking.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+
+        }
+        public Song(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
